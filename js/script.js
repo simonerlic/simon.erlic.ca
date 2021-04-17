c0366de98b922891c3dac76eb2db3f1da6d800ed
simonerlic <simon@erlic.ca>
2021-04-16 23:08:49 -0700

    Fix date function

--- js/script.js
+++ js/script.js
@@ -8,9 +8,7 @@ function realTimeClock() {
 	var hours = clock.getHours();
 	var minutes = clock.getMinutes();
 	var seconds = clock.getSeconds();
-	var day = clock.getDay();
-	var month = clock.getMonth();
-	var year = clock.getFullYear();
+	var date = new Date().toJSON().slice(0,10).replace(/-/g,'/');
 	
 	// am or pm?
 	var tod = ( hours < 12 ) ? "am" : "pm";
@@ -22,12 +20,10 @@ function realTimeClock() {
 	hours = ("0" + hours).slice(-2);
 	minutes = ("0" + minutes).slice(-2);
 	seconds = ("0" + seconds).slice(-2);
-	day = ("0" + day).slice(-2);
-	month = ("0" + month).slice(-2);
 	
 	// actually show the clock
 	document.getElementById('clock').innerHTML = hours + " : " + minutes + " : " + seconds;
 	document.getElementById('ampm').innerHTML = tod;
-	document.getElementById('date').innerHTML = day + " / " + month + " / " + year;
+	document.getElementById('date').innerHTML = date;
 	var t = setTimeout( realTimeClock, 500 );
 }
